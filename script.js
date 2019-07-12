3d7223c4a5c0f12e11e2a23ca3c8abab36fddfb3
Niels G. W. Serup <ngws@metanohi.name>
2019-07-12 20:40:27 +0200

    Add more slogans.

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ function set_slogan() {
     'Hvorfor oste-pive når du kan oste-skive?',
     'Aubergine i min burger?  Hvorfor ikke!',
     'burge: verbum (burger, burgede, burget)',
-    '50000 burgere, tak!'
+    '50000 burgere, tak!',
+    'Kan jeg bestille en nummer 21 med ekstra ost?',
+    'Øøøøøøøøøøøøøøøøøøøh... BURGER!',
+    'Så for pape, for at spise den skal jeg gabe!',
+    'Bim, bam, brioche, at spise burger vil jeg også.'
   ],
       slogan = slogans[Math.floor(Math.random() * slogans.length)];
 
