35059d6023caf87eaec00dc71c0c2c242c106e82
Niels G. W. Serup <ngws@metanohi.name>
2019-07-12 21:10:55 +0200

    Improve header and footer.

--- generate_html_overview.py
+++ generate_html_overview.py
@@ -4,6 +4,7 @@ import os
 import json
 import html
 import re
+import subprocess
 
 def make_prop(id, s):
     return '\n'.join('<div><a href="/se-og-stoet-forslag/?Id=FT-{}">{}</a></div>'.format(id, line) for line in s.split('\n'))
@@ -16,7 +17,8 @@ proposals = [make_prop(fn.split('.')[0], html.unescape(json.load(
              for fn in fnames]
 
 header = open(os.path.join(basedir, 'templates', 'header.html')).read()
-footer = open(os.path.join(basedir, 'templates', 'footer.html')).read()
+newest_commit = subprocess.run(['git', '-C', basedir, 'log', '--pretty=format:%h', 'HEAD^..HEAD'], stdout=subprocess.PIPE).stdout.decode().strip()
+footer = open(os.path.join(basedir, 'templates', 'footer.html')).read().format(newest_commit=newest_commit)
 
 with open(os.path.join(basedir, 'templates', 'forside.html')) as f:
     print(f.read().format(proposals='\n'.join(proposals),
--- generate_html_proposal.py
+++ generate_html_proposal.py
@@ -5,6 +5,7 @@ import os
 import json
 import html
 import re
+import subprocess
 
 def make_paragraphs(s):
     return '\n'.join('<p>{}</p>'.format(line) for line in s.split('\n'))
@@ -15,7 +16,8 @@ with open(os.path.join(basedir, 'text', id + '.json')) as f:
     data = json.load(f)
 
 header = open(os.path.join(basedir, 'templates', 'header.html')).read()
-footer = open(os.path.join(basedir, 'templates', 'footer.html')).read()
+newest_commit = subprocess.run(['git', '-C', basedir, 'log', '--pretty=format:%h', 'HEAD^..HEAD'], stdout=subprocess.PIPE).stdout.decode().strip()
+footer = open(os.path.join(basedir, 'templates', 'footer.html')).read().format(newest_commit=newest_commit)
 
 with open(os.path.join(basedir, 'templates', 'side.html')) as f:
     html = f.read().format(title=html.escape(data['title']), id=id,
